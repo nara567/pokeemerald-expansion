5450f5dd6585864f0d31e1e251d1e6e996d535d0
nara567 <bgavilan27@gmail.com>
2026-09-12 12:57:28 -0500

    Slateport Gym + Other Leaders

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1740,3 +1740,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/wild_encounter.inc"
 
 	.include "data/maps/Route103_2/scripts.inc"
+
+	.include "data/maps/SlateportCity_Gym/scripts.inc"
