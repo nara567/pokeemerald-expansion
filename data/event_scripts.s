f64f13a4100335bc690918a7e8d9c0847d843202
nara567 <bgavilan27@gmail.com>
2026-09-16 08:40:24 -0500

    new maps

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1752,3 +1752,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/LittlerootTown_RivalHouse_2F/scripts.inc"
 
 	.include "data/maps/LittlerootClearing/scripts.inc"
+
+	.include "data/maps/SlateportBeach/scripts.inc"
